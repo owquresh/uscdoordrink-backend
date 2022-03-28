db48ff8eb97338580b74cc66fc3edd8df2e3baad
Hanna Kishawi <kishawi@usc.edu>
2022-03-27 21:38:16 -0700

    Add dummy data for stores into sql file

--- uscdoordrink.sql
+++ uscdoordrink.sql
@@ -61,3 +61,11 @@ create table if not exists orderitems(
 	foreign key(orderid) references orders(orderid)
 
 );
+INSERT INTO shops(name, email, password, address, state, lat, lng, postal, city)
+VALUES("shop 1", "shop1@gmail", "123", "2810 S Figueroa St", "CA", 34.026192, -118.276993, "9007", "Los Angeles"); 
+INSERT INTO shops(name, email, password, address, state, lat, lng, postal, city)
+VALUES("shop 2", "shop2@gmail", "123", "2701 S Vermont Ave", "CA", 34.030041, -118.292000, "9007", "Los Angeles"); 
+INSERT INTO shops(name, email, password, address, state, lat, lng, postal, city)
+VALUES("shop 3", "shop3@gmail", "123", "1101 W 23rd St", "CA", 34.035049, -118.283539, "9007", "University Park"); 
+INSERT INTO shops(name, email, password, address, state, lat, lng, postal, city)
+VALUES("shop 4", "shop4@gmail", "123", "3758 S Figueroa St", "CA", 34.016788, -118.282722, "9007", "Los Angeles"); 
