756298fa18acdc2f74de6030e1a121780e14226d
kishawi <kishawi@usc.edu>
2022-03-28 21:54:13 -0700

    set up customers getting from database

--- USCDoorDrinkBackend/src/main/java/models/Customer.java
+++ USCDoorDrinkBackend/src/main/java/models/Customer.java
@@ -2,11 +2,79 @@ package models;
 
 import java.util.ArrayList;
 
-public class Customer extends Client{
-	public Customer(String username, String name, String password, String address, int id) {
-		super(username, name, password, address, id);
-		
+public class Customer {
+	private String name;
+	private String email;
+	private String pass;
+	private int id;
+	private String address;
+	private String state;
+	private String postal;
+	private String city;
+	private double lat;
+	private double lng;
+	public Customer(String name, String email, String pass, String address, String state, String city, String postal) {
+		super();
+		this.name = name;
+		this.pass = pass;
+		this.city = city;
+		this.email = email;
+		this.id = id;
+		this.address = address;
+		this.state = state;
+		this.postal = postal;
+		this.lat = lat;
+		this.lng = lng;
 	}
-	private ArrayList<Order> orderHistory;
-	private ArrayList<Item> cart;	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {return name;}
+	public String getPass() {return pass;}
+	public String getCity() {return city;}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPostal() {
+		return postal;
+	}
+	public void setPostal(String postal) {
+		this.postal = postal;
+	}
+	public double getLat() {
+		return lat;
+	}
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+	public double getLng() {
+		return lng;
+	}
+	public void setLng(double lng) {
+		this.lng = lng;
+	}
+
+
+
+//	private ArrayList<Order> orderHistory;
+//	private ArrayList<Item> cart;
 }
